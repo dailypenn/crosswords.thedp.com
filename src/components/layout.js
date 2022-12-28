b7d82ee4d069674ea7b1325c52454539b48ae964
Ethan Weisberg <eiweisberg.2718@gmail.com>
2022-12-28 10:33:38 -0500

    main page

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -32,6 +33,9 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
+      </div>
+      <div id="footer">
+        <Footer></Footer>
       </div>
     </>
   )
